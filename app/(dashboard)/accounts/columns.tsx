5e43e1a33034383eaf0bf0f4adcedc1c7afd1484
timiomotayo <oluwatimilehintheophilus@gmail.com>
2025-09-16 13:12:58 +0100

    edit account and single delete functionality

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -1,13 +1,16 @@
 "use client"
 
-import { InferResponseType } from "hono"
-import { client } from "@/lib/hono"
-import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
+import { InferResponseType } from "hono";
+import { ArrowUpDown } from "lucide-react";
+import { ColumnDef } from "@tanstack/react-table";
 
-type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0]
+import { client } from "@/lib/hono";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import { Actions } from "./actions";
+
+export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0];
 
 export const columns: ColumnDef<ResponseType>[] = [
   {
@@ -29,10 +32,12 @@ export const columns: ColumnDef<ResponseType>[] = [
         aria-label="Select row"
       />
     ),
+    enableSorting: false,
+    enableHiding: false,
   },
   {
     accessorKey: "name",
-        header: ({ column }) => {
+    header: ({ column }) => {
       return (
         <Button
           variant="ghost"
@@ -42,6 +47,10 @@ export const columns: ColumnDef<ResponseType>[] = [
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
-    },
+    }
   },
-]
+  {
+    id: "actions",
+    cell: ({ row }) => <Actions id={row.original.id} />
+  }
+];
