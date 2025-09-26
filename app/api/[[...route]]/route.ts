e89f210b338458f8fc98927f7f64f4e40646cb78
timiomotayo <oluwatimilehintheophilus@gmail.com>
2025-09-26 03:31:59 +0100

    transactions logic, database migration and api endpoints

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -7,10 +7,12 @@ export const runtime = "edge";
 
 import accounts from './accounts'
 import categories from './categories'
+import transactions from './transactions'
 
 const routes = app
   .route('/accounts', accounts)
   .route('/categories', categories)
+  .route('/transactions', transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
