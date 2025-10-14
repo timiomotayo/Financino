48ce7a9ef4e91642678a5deceae6b9aa0b145ca5
Timi Omotayo <70233949+timiomotayo@users.noreply.github.com>
2025-10-14 22:51:58 +0100

    Dev transactions (#4)
    * transactions logic, database migration and api endpoints
    
    * transactions hooks and bulk-create transactions endpoint
    
    * build transaction form and handle amount/currency logic
    
    * transaction page  content and data

--- features/transactions/api/use-edit-transaction.ts
+++ features/transactions/api/use-edit-transaction.ts
@@ -4,10 +4,10 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/hono";
 
-type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$patch"]>;
-type RequestType = InferRequestType<typeof client.api.accounts[":id"]["$patch"]>["json"];
+type ResponseType = InferResponseType<typeof client.api.transactions[":id"]["$patch"]>;
+type RequestType = InferRequestType<typeof client.api.transactions[":id"]["$patch"]>["json"];
 
-export const useEditAccount = (id?: string) => {
+export const useEditTransaction = (id?: string) => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<
@@ -16,20 +16,19 @@ export const useEditAccount = (id?: string) => {
     RequestType
   >({
     mutationFn: async (json) => {
-      const response = await client.api.accounts[":id"]["$patch"]({ 
+      const response = await client.api.transactions[":id"]["$patch"]({ 
         param: { id },
         json,
       });
       return await response.json();
     },
     onSuccess: () => {
-      toast.success("Account updated");
-      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
-      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      toast.success("Transaction updated");
+      queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
       queryClient.invalidateQueries({ queryKey: ["transactions"] });
     },
     onError: () => {
-      toast.error("Failed to edit account");
+      toast.error("Failed to edit transaction");
     },
   });
 
