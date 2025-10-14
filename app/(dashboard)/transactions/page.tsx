6b5823c4289f304bf05aed363bfe8c27315feecd
Timi Omotayo <70233949+timiomotayo@users.noreply.github.com>
2025-10-14 23:09:39 +0100

    Dev transactions (#8)
    * import transactions from csv file feature (#6)
    
    * Dev summary api (#7)
    
    * import transactions from csv file feature
    
    * api endpoint and logics for summary data

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { useState } from "react";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
 
+import { useSelectAccount } from "@/features/accounts/hooks/use-select-account";
+
+import { transactions as transactionSchema } from "@/db/schema";
+
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Loader2, Plus } from "lucide-react";
 
@@ -17,17 +24,66 @@ import {
 import { columns } from "./columns"
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ImportCard } from "./import-card";
+import { UploadButton } from "./upload-button";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT"
+};
+
+const INITIAL_IMPORT_RESULTS = { 
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 const TransactionsPage = () => {
-  const newTransaction = useNewTransaction();
-  const transactionsQuery = useGetTransactions();
-  const deleteTransactions = useBulkDeleteTransactions();
+  const [AccountDialog, confirm] = useSelectAccount();
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
 
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    console.log({ results });
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
+  const newTransaction = useNewTransaction();
+  const createTransactions = useBulkCreateTransactions();
+  const deleteTransactions = useBulkDeleteTransactions();
+  const transactionsQuery = useGetTransactions();
   const transactions = transactionsQuery.data || [];
 
   const isDisabled =
     transactionsQuery.isLoading ||
     deleteTransactions.isPending;
+
+  const onSubmitImport = async (
+    values: typeof transactionSchema.$inferInsert[],
+  ) => {
+    const accountId = await confirm();
+
+    if (!accountId) {
+      return toast.error("Please select an account to continue.");
+    }
+
+    const data = values.map((value) => ({
+      ...value,
+      accountId: accountId as string,
+    }));
+
+    createTransactions.mutate(data, {
+      onSuccess: () => {
+        onCancelImport();
+      },
+    });
+  };
 
   if (transactionsQuery.isLoading) {
     return (
@@ -45,6 +101,19 @@ const TransactionsPage = () => {
       </div>
     );
   }
+
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <AccountDialog />
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={onSubmitImport}
+        />
+      </>
+    );
+  }
   
   return ( 
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -53,10 +122,17 @@ const TransactionsPage = () => {
           <CardTitle className="text-xl line-clamp-1">
             Transaction History
           </CardTitle>
-          <Button onClick={newTransaction.onOpen} size="sm" className="w-full lg:w-auto">
-            <Plus className="size-4 mr-2" />
-            Add new
-          </Button>
+          <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2 w-full lg:w-auto">
+            <Button 
+              onClick={newTransaction.onOpen} 
+              size="sm"
+              className="w-full lg:w-auto"
+            >
+              <Plus className="size-4 mr-2" />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
