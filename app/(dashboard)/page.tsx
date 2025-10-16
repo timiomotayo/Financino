fc93843a8322b26642c6aa1e3f60716ab5f48d39
timiomotayo <oluwatimilehintheophilus@gmail.com>
2025-10-16 04:52:57 +0100

    dashboard components, summary data and charts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,11 @@
-"use client";
+import { DataGrid } from "@/components/data-grid";
+import { DataCharts } from "@/components/data-charts";
 
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-
-export default function Home() {
-  const { onOpen } = useNewAccount();
-
+export default function DashboardPage() {
   return (
-    <div className="flex flex-cols items-center justify-center min-h-screen gap-4">
-      <Button onClick={onOpen}>Add new account</Button>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
+      <DataCharts />
     </div>
   );
-}
+};
