8aa003249ad29eb8e0eff94d87e41a1c539ad323
timiomotayo <oluwatimilehintheophilus@gmail.com>
2025-10-17 23:05:17 +0100

    prepare app for deployment and fix build errors

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,17 @@
-import { DataGrid } from "@/components/data-grid";
-import { DataCharts } from "@/components/data-charts";
+import { Suspense } from "react";
+
+import { DataGrid, DataGridSkeleton } from "@/components/data-grid";
+import { DataCharts, DataChartsSkeleton } from "@/components/data-charts";
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-      <DataGrid />
-      <DataCharts />
+      <Suspense fallback={<DataGridSkeleton />}>
+        <DataGrid />
+      </Suspense>
+      <Suspense fallback={<DataChartsSkeleton />}>
+        <DataCharts />
+      </Suspense>
     </div>
   );
 };
--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -5,20 +5,24 @@ import { useGetSummary } from "@/features/summary/api/use-get-summary";
 import { Chart, ChartLoading } from "@/components/chart";
 import { SpendingPie, SpendingPieLoading } from "@/components/spending-pie";
 
+export const DataChartsSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <ChartLoading />
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        <SpendingPieLoading />
+      </div>
+    </div>
+  );
+};
+
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary();  
 
   if (isLoading) {
-    return (
-      <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
-        <div className="col-span-1 lg:col-span-3 xl:col-span-4">
-          <ChartLoading />
-        </div>
-        <div className="col-span-1 lg:col-span-3 xl:col-span-2">
-          <SpendingPieLoading />
-        </div>
-      </div>
-    );
+    return <DataChartsSkeleton />;
   }
 
   return (
